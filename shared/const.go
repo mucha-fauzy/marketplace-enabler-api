0b535681e34589c91d76285f928def0e378c5ad7
mucha-fauzy <muchamad.fauzy@evermos.com>
2023-09-22 13:43:29 +0700

    feat: add const parameter for pagination

--- shared/const.go
+++ shared/const.go
@@ -8,7 +8,14 @@ const (
 	EndDateQueryField   = "dateTo"
 	BrandNameQueryField = "brand"
 	StoreNameQueryField = "store"
+	PageQueryField      = "page"
+	SizeQueryField      = "size"
 	DefaultDateFormat   = time.RFC3339
+	DefaultPage         = 1
+	DefaultSize         = 10
+	PageLowerLimit      = 1
+	SizeLowerLimit      = 1
+	SizeUpperLimit      = 20
 	BLIBLI              = "blibli"
 	SHOPEE              = "shopee"
 )
